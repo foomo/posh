8b1224ca226cc21b606b20766f010b3b4186a40d
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2023-01-09 09:58:17 +0100

    feat: add config command

---
+++ cmd/config.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	intconfig "github.com/foomo/posh/internal/config"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:           "config",
+	Short:         "Print loaded configuration",
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if err := intconfig.Load(l); err != nil {
+			return err
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		viper.Debug()
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
