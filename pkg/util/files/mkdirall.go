c258cdcc495499da700c503e5b94cc6f90dead96
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2023-02-14 17:29:33 +0100

    feat: check empty name

--- pkg/util/files/mkdirall.go
+++ pkg/util/files/mkdirall.go
@@ -8,6 +8,9 @@ import (
 
 func MkdirAll(paths ...string) error {
 	for _, path := range paths {
+		if path == "" {
+			return errors.New("invalid empty path")
+		}
 		if stat, err := os.Stat(path); err != nil && os.IsNotExist(err) {
 			if err := os.MkdirAll(path, os.ModeDir|0700); err != nil {
 				return err
