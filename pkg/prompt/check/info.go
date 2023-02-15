42badacdbc0c797275b09ca2dedc3d31ec9140b0
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2023-02-15 10:50:48 +0100

    feat: add helper

--- pkg/prompt/check/info.go
+++ pkg/prompt/check/info.go
@@ -5,3 +5,19 @@ type Info struct {
 	Note   string
 	Status Status
 }
+
+func NewSuccessInfo(name, note string) Info {
+	return Info{
+		Name:   name,
+		Note:   note,
+		Status: StatusSuccess,
+	}
+}
+
+func NewFailureInfo(name, note string) Info {
+	return Info{
+		Name:   name,
+		Note:   note,
+		Status: StatusFailure,
+	}
+}
