1cdc2edb654030f078677277f7d388148044c32c
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-04-29 08:30:02 +0200

    fix: lint issues

--- pkg/util/browser/open.go
+++ pkg/util/browser/open.go
@@ -25,9 +25,10 @@ func OpenURL(ctx context.Context, u *url.URL) error {
 
 	switch runtime.GOOS {
 	case "linux":
-		if isWSL() {
+		if isWSL(ctx) {
 			return exec.CommandContext(ctx, "powershell.exe", "Start-Process", u.String()).Start()
 		}
+
 		return exec.CommandContext(ctx, "xdg-open", u.String()).Start()
 	case "windows":
 		return exec.CommandContext(ctx, "rundll32", "url.dll,FileProtocolHandler", u.String()).Start()
@@ -38,10 +39,11 @@ func OpenURL(ctx context.Context, u *url.URL) error {
 	}
 }
 
-func isWSL() bool {
-	data, err := exec.Command("uname", "-r").Output()
+func isWSL(ctx context.Context) bool {
+	data, err := exec.CommandContext(ctx, "uname", "-r").Output()
 	if err != nil {
 		return false
 	}
+
 	return strings.Contains(strings.ToLower(string(data)), "microsoft")
 }
