bf12e4aebc74abb742fe76200beb85af85482958
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-05-11 08:37:57 +0200

    fix: lint issues

--- pkg/prompt/check/defaultcheck.go
+++ pkg/prompt/check/defaultcheck.go
@@ -36,21 +36,25 @@ func DefaultCheck(ctx context.Context, l log.Logger, checkers []Checker) error {
 				switch info.Status {
 				case StatusFailure:
 					color = pterm.FgRed
+
 					if info.Icon == "" {
 						info.Icon = "\uF467"
 					}
 				case StatusWarning:
 					color = pterm.FgYellow
+
 					if info.Icon == "" {
 						info.Icon = "\uEA74"
 					}
 				case StatusSuccess:
 					color = pterm.FgGreen
+
 					if info.Icon == "" {
 						info.Icon = "\uEAB2"
 					}
 				default:
 					color = pterm.FgGray
+
 					if info.Icon == "" {
 						info.Icon = "\U000F0131"
 					}
